287348bf9f9672696b47b31d79a50a384fae04f6
socz3qqq <soczawa@student.agh.edu.pl>
2022-07-25 12:29:17 +0200

    First test

--- Pytest/Task1/test_function.py
+++ Pytest/Task1/test_function.py
@@ -1,2 +1,8 @@
 from function import square
+
+def test_1():
+    assert square(0) == 0
+    assert square(1) == 1
+    assert square(-1) == 1
+    assert square(100) == 10000
 # Napisać cztery testy z dla funckji square dla 0, 1, -1 i 100
